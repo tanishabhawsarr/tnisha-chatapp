86e000a279ad728ed935c1f320cda971f8b047d3
tanishabhawsarr <156940982+tanishabhawsarr@users.noreply.github.com>
2024-08-04 09:57:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,15 +40,7 @@ cloudinary.config({
 const app=express();
 const server=createServer(app);
 const io=new Server(server, {
-  cors: {
-        origin: [
-            "http://localhost:5173",
-            "http://localhost:4173",
-            process.env.CLIENT_URL,
-        ],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true,
-    },
+  cors: corsOptions,
 });
 
 app.set("io",io);
