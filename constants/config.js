608baf0aa62fa84660f1c2f3e5770a80fbadbc5c
tanishabhawsarr <156940982+tanishabhawsarr@users.noreply.github.com>
2024-08-04 09:55:06 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,11 +1,13 @@
 const corsOptions={
-        origin:[
-          "http://localhost:5173",
-          "http://localhost:4173",
-          process.env.CLIENT_URL,
-        ],
-        method:["GET","POST","PUT","DELETE"],
-        credentials:true,
+       origin: [
+        "http://localhost:5173",
+        "http://localhost:4173",
+        process.env.CLIENT_URL,
+       https://tnisha-chatapp-frontend.vercel.app/ 
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
 }
 
 const LETSTALK_TOKEN="letstalk-token"
